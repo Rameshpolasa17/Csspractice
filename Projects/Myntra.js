7e3ba2858b006aa863ce5fb60af2cd0c29aacb97
Rameshpolasa17 <149149368+Rameshpolasa17@users.noreply.github.com>
2023-11-18 16:16:33 +0000

    commit

---
+++ Projects/Myntra.js
@@ -0,0 +1,46 @@
+let bagItems=[];
+onLoad();
+
+function onLoad(){
+    displayItemsOnHomePage()
+    displayBagIcon()
+}
+
+function addtoBag(itemId){
+    bagItems.push(itemId)
+    displayBagIcon()
+}
+
+function displayBagIcon() {
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if (bagItems.length > 0) {
+      bagItemCountElement.style.visibility='visible'
+      bagItemCountElement.innerText = bagItems.length;
+    } else {
+      bagItemCountElement.style.visibility='hidden'
+    }
+  }
+
+function displayItemsOnHomePage(){
+    let itemContainer=document.querySelector('.items_container')
+    let innerHtml=``
+    items.forEach(item=>{
+        innerHtml+=`
+        <div class="item_container">
+            <img class="item_img" src="${item.item_img}">
+            <div class="rating">
+                ${item.rating.stars} | ${item.rating.noOfReviews} 
+            </div>
+            <div class="brand_name">${item.brand_name}</div>
+            <div class="item_name">${item.item_name}</div>
+            <div class="price">
+                <span class="current_price">RS ${item.current_price}</span>
+                <span class="original_price">RS ${item.original_price}</span>
+                <span class="discount">(${item.discount}% OFF)</span>
+            </div>
+            <button class="add_bag" onclick="addtoBag(${item.id})">Add to Bag</button>
+        </div>`
+})
+
+itemContainer.innerHTML= innerHtml
+}
---
+++ Projects/data.js
@@ -0,0 +1,78 @@
+const items=[
+    {
+        id:'01',
+        item_img:`https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/22090122/2023/2/23/a6999277-79d6-4d67-9645-123d4403259b1677129300172MelangebyLifestyleKurtaSetsViscose1.jpg`,
+
+        rating:{
+        stars:"4.3⭐",
+        noOfReviews:1400
+        },
+        brand_name:"Melange by Lifestyle",
+        item_name:'Floral Printed Straight Kurta with Trousers',
+        current_price:'799',
+        original_price:'2599',
+        discount:65
+    },
+    
+    {
+        id:'02',
+        item_img:`https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/24230266/2023/10/19/54db6d1c-7ff2-47a9-8c54-c3c1ff9b65f01697716903405-Force-IX-Men-Regular-fit-Short-Sleeve-Crew-Neck-Tshirts-6716-1.jpg`,
+
+        rating:{
+        stars:"4.1⭐",
+        noOfReviews:1800
+        },
+        brand_name:"Nautica",
+        item_name:'Graphic Printed Pure Cotton Applique T-shirt',
+        current_price:'599',
+        original_price:'1199',
+        discount:45
+    },
+    
+    {
+        id:'03',
+        item_img:`https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/22967104/2023/5/2/8dd83210-ae1b-487c-8c4f-97caf8d555741683003694455MenBlackStretchSlimFitChinoPant1.jpg`,
+
+        rating:{
+        stars:"4.4⭐",
+        noOfReviews:600
+        },
+        brand_name:"Nautica",
+        item_name:'Men Mid-Rise Slim Fit Chinos',
+        current_price:'999',
+        original_price:'1999',
+        discount:50
+    },
+
+    {
+        id:'04',
+        item_img:`https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/25093284/2023/9/21/1de9b36b-256f-498b-83df-a6bb20527d7a1695272241361LilPicksGirlsPinkGreenPrintedReadytoWearLehengaBlouseWithDup5.jpg`,
+
+        rating:{
+        stars:"4.6⭐",
+        noOfReviews:2600
+        },
+        brand_name:"LilPicks",
+        item_name:'Girls Ethnic Motifs Printed Ready to Wear Lehenga & Blouse With Dupatta',
+        current_price:'1249',
+        original_price:'3499',
+        discount:63
+    },
+
+    {
+        id:'05',
+        item_img:`https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/24670120/2023/9/12/e0e6315f-a5f0-4f60-b76d-b266bf2bd4711694485532260BownBeeGirlsGreenRedPrintedFoilPrintReadytoWearLehengaBlouse1.jpg`,
+
+        rating:{
+        stars:"4.7⭐",
+        noOfReviews:4300
+        },
+        brand_name:"BownBee",
+        item_name:'Girls Ethnic Motifs Foil Printed Ready to Wear Lehenga Choli',
+        current_price:'2099',
+        original_price:'3499',
+        discount:45
+    },
+
+    
+]
